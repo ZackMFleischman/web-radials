df403a9dc09c1446489f338b29ba071d5dc61132
Zack M Fleischman <zFleischman@codecademy.com>
2021-10-25 01:09:10 -0500

    Bezier curve rendering

--- src/RadialsCanvas.tsx
+++ src/RadialsCanvas.tsx
@@ -18,6 +18,10 @@ export const RadialsCanvas = () => {
       const randomColor = Math.random() > 0.5 ? "#ff8080" : "#0099b0";
       ctx.fillStyle = randomColor;
       ctx.fillRect(100, 50, 100, 17);
+
+      ctx.strokeStyle = randomColor;
+      ctx.lineWidth = 2;
+      renderCurve(ctx);
     },
     []
   );
@@ -25,4 +29,11 @@ export const RadialsCanvas = () => {
   const canvasRef = useCanvasRenderer(renderFrame);
 
   return <StyledCanvas id="radials-canvas" ref={canvasRef} />;
+};
+
+const renderCurve = (ctx: CanvasRenderingContext2D) => {
+  ctx.beginPath();
+  ctx.moveTo(30, 30);
+  ctx.bezierCurveTo(120, 160, 180, 10, 220, 140);
+  ctx.stroke();
 };
