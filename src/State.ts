a99ee9bf0ed560093be2d95b8b13ea45c68740ad
Zack M Fleischman <zFleischman@codecademy.com>
2022-08-10 14:20:25 -0400

    Updating defaults

--- src/State.ts
+++ src/State.ts
@@ -4,26 +4,33 @@ import { DropdownOption } from "./Dropdown";
 import randomColor from "randomcolor";
 
 export const startingCPVelocity = 1.0;
-const curveSizeToCPSizeRatio = 7.0/5.0;
+const curveSizeToCPSizeRatio = 7.0 / 5.0;
 
-export const clamp = (x: number, value: number) => Math.max(Math.min(x, value), -value);
+export const clamp = (x: number, value: number) =>
+  Math.max(Math.min(x, value), -value);
 
-export type Luminosity = "normal" | "random" | "dark" | "light" | "bright" | undefined;
+export type Luminosity =
+  | "normal"
+  | "random"
+  | "dark"
+  | "light"
+  | "bright"
+  | undefined;
 
 export class State {
   maxCurveSize = 400;
-  maxControlPointSize = Math.round(this.maxCurveSize*curveSizeToCPSizeRatio);
+  maxControlPointSize = Math.round(this.maxCurveSize * curveSizeToCPSizeRatio);
   maxCPVelocity = 3;
 
   maxScaleDelta = 0.25;
   scale = 1;
   scaleVelocity = 0.2;
 
-  curveWidth = 4;
+  curveWidth = 2;
 
-  numRadials = 15;
-  skipEveryNthRadial = 0;
-  numberOfRadialsToSkip = 1;
+  numRadials = 56;
+  skipEveryNthRadial = 8;
+  numberOfRadialsToSkip = 3;
 
   windowSizeProportion = 0.7;
 
@@ -37,34 +44,38 @@ export class State {
   c2xVelocity: number;
   c2yVelocity: number;
 
-  spin = 0;
-  spinVelocity = 0.1;
+  spin = -0.05;
+  spinVelocity = -0.15;
 
   showControlLines = false;
 
   colorPalette: string[];
-  colorPaletteSize = 1;
-  colorHue?: DropdownOption = { value: "white", label: "White" };
-  colorLuminosity?: DropdownOption<Luminosity> = { value: "normal", label: "Normal" };
+  colorPaletteSize = 5;
+  colorHue?: DropdownOption = { value: "blue", label: "Blue" };
+  colorLuminosity?: DropdownOption<Luminosity> = {
+    value: "light",
+    label: "Light",
+  };
 
   renderControls: () => void;
 
   constructor() {
     this.setMaxCurveSize(400);
 
-    this.colorPalette = ["white"];
+    this.colorPalette = ["blue"];
+    this.generateColorPalette();
 
     this.c1x = 90;
     this.c1y = 130;
     this.c2x = 150;
     this.c2y = -20;
 
-    this.c1xVelocity = 1.0;
-    this.c1yVelocity = 1.2;
-    this.c2xVelocity = 1.4;
-    this.c2yVelocity = 1.6;
+    this.c1xVelocity = 2.0;
+    this.c1yVelocity = 2.2;
+    this.c2xVelocity = 2.4;
+    this.c2yVelocity = 2.6;
 
-    this.renderControls = () => {}
+    this.renderControls = () => {};
   }
 
   setNumRadials(numRadials: number) {
@@ -73,7 +84,9 @@ export class State {
 
   setMaxCurveSize(maxCurveSize: number) {
     this.maxCurveSize = maxCurveSize;
-    this.maxControlPointSize = Math.round(this.maxCurveSize*curveSizeToCPSizeRatio);
+    this.maxControlPointSize = Math.round(
+      this.maxCurveSize * curveSizeToCPSizeRatio
+    );
   }
 
   setC1x(c1x: number) {
@@ -89,16 +102,28 @@ export class State {
     this.c2y = clamp(c2y, this.maxControlPointSize);
   }
   setC1xVelocity(vel: number) {
-    this.c1xVelocity = clamp(this.c1xVelocity >= 0 ? vel : -vel, this.maxCPVelocity);
+    this.c1xVelocity = clamp(
+      this.c1xVelocity >= 0 ? vel : -vel,
+      this.maxCPVelocity
+    );
   }
   setC1yVelocity(vel: number) {
-    this.c1yVelocity = clamp(this.c1yVelocity >= 0 ? vel : -vel, this.maxCPVelocity);
+    this.c1yVelocity = clamp(
+      this.c1yVelocity >= 0 ? vel : -vel,
+      this.maxCPVelocity
+    );
   }
   setC2xVelocity(vel: number) {
-    this.c2xVelocity = clamp(this.c2xVelocity >= 0 ? vel : -vel, this.maxCPVelocity);
+    this.c2xVelocity = clamp(
+      this.c2xVelocity >= 0 ? vel : -vel,
+      this.maxCPVelocity
+    );
   }
   setC2yVelocity(vel: number) {
-    this.c2yVelocity = clamp(this.c2yVelocity >= 0 ? vel : -vel, this.maxCPVelocity);
+    this.c2yVelocity = clamp(
+      this.c2yVelocity >= 0 ? vel : -vel,
+      this.maxCPVelocity
+    );
   }
 
   setScaleVelocity(vel: number) {
@@ -121,7 +146,10 @@ export class State {
         : randomColor({
             count: this.colorPaletteSize,
             hue: this.colorHue?.value ?? "random",
-            luminosity: this.colorLuminosity?.value === "normal" ? undefined : this.colorLuminosity?.value
+            luminosity:
+              this.colorLuminosity?.value === "normal"
+                ? undefined
+                : this.colorLuminosity?.value,
           });
 
     this.colorPaletteSize = this.colorPalette.length;
